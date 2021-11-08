bc3887f0fc79b89e340ece9f6aabe7cf553fd562
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2021-11-08 23:23:40 +0800

    Added animations and changed photo

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const HomeWrapper = styled.div`
   display: flex;
@@ -6,29 +6,64 @@ const HomeWrapper = styled.div`
   margin-top: 60px;
   justify-content: space-around;
   user-select: none;
+  margin-top: 5%;
 `;
 
 const GreetingsWrapper = styled.div`
-  padding: 20px;
+  padding-left: 100px;
+  height: 50vh;
+  width: 100%;
+  margin-top: 10em;
+`;
+
+const typewriter = keyframes`
+  from { width: 0 }
+  to { width: 100% }
 `;
 
 const Greetings = styled.h1`
+  margin-top: 0;
+  margin-bottom: 0;
   font-size: 68px;
-  animation: move-text 0.75s forwards;
+  overflow: hidden;
+  white-space: nowrap;
+  animation: ${typewriter} 1.5s steps(30, end);
 `;
 
 const Name = styled(Greetings)`
   font-size: 80px;
-  animation: move-text 0.75s forwards;
+  animation-name: ${typewriter};
+  animation-delay: 1.1s;
+  animation-fill-mode: both;
+  margin-bottom: 20px;
 `;
 
-const ImageWrapper = styled.div``;
+const Role = styled(Greetings)`
+  animation-name: ${typewriter};
+  animation-delay: 2.2s;
+  animation-fill-mode: both;
+`;
+
+const ImageWrapper = styled.div`
+  width: 100%;
+  margin-top: 5em;
+`;
+
+const fadeIn = keyframes`
+  from { opacity: 0 }
+  to { opacity: 1 }
+`;
 
 const ImageContainer = styled.div`
-  height: 650px;
-  width: 500px;
+  height: 600px;
   background-size: cover;
-  background-image: url("/images/me.JPG");
+  background-image: url("/images/memoji.PNG");
+  transform: translateY(-100px);
+  background-repeat: no-repeat;
+  background-position: center center;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
 `;
 
 const Home = () => {
@@ -37,11 +72,10 @@ const Home = () => {
       <GreetingsWrapper>
         <Greetings>Hello World! 👋🏼</Greetings>
         <Name>I'm Chris John,</Name>
-        <Greetings>Full Stack Developer.</Greetings>
+        <Role>Full Stack Developer.</Role>
       </GreetingsWrapper>
       <ImageWrapper>
         <ImageContainer />
-        {/*<img src="/images/me.JPG" />*/}
       </ImageWrapper>
     </HomeWrapper>
   );
